4c9f66504620a939fc58e66f512bb714f1a76c7d
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-10-23 16:00:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(sentence1)
   reversed = []
   sentence1.each do |word|
-    reversed << "#{word}".reverse
+    reversed << "#{word}"
   end
-  sentence1
+  sentence1.reverse
 end
 
 # def hamburger(toppings)
