fd9891e4ee7f919c7a7aaee7b11b25a4f2469f5b
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-10-23 16:27:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,10 +6,3 @@ def reverse_each_word(sentence1)
   reversed.join(" ")
 end
 
-# def hamburger(toppings)
-#   my_statements = []
-#   toppings.each do |topping|
-#     my_statements << "I love #{topping} on my burgers!"
-#   end
-#   my_statements
-# end
