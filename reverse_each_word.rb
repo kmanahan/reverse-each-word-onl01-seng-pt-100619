5b25141600c277a56ebcc59e2d537f3427b7a45a
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-10-23 16:46:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,10 @@
-def reverse_each_word(sentence1,sentence2)
-  # reversed = []
-  # sentence1.split(' ').each do |word|
-  #   reversed << word.reverse
-  # end
-  # reversed.join(" ")
-  array.split(" ").collect {|x| x.reverse!}.join(" ")
-end
+# def reverse_each_word(sentence1)
+#   reversed = []
+#   sentence1.split(' ').each do |word|
+#     reversed << word.reverse
+#   end
+#   reversed.join(" ")
+# end
 
+def reverse_each_word(sentence12)
+  
